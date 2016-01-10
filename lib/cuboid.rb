66b900370691f3407db2ec7785d992ee30479ca2
Silvio Huebner-Schurch <silvio@vicinitycommerce.com>
2016-01-09 22:33:29 -0800

    check if vertex of one cuboid is inside another

--- lib/cuboid.rb
+++ lib/cuboid.rb
@@ -1,21 +1,13 @@
 
 class Cuboid
 
-  #BEGIN public methods that should be your starting point
-
   def initialize(params = {})
     @x = params[:x]
     @y = params[:y]
     @z = params[:z]
-    @length = params[:length]
-    @width = params[:width]
-    @height = params[:height]
-  end
-
-  def move_to!(x, y, z)
-    @x = x
-    @y = y
-    @z = z
+    @length = params[:length].abs
+    @width = params[:width].abs
+    @height = params[:height].abs
   end
 
   def vertices
@@ -31,15 +23,38 @@ class Cuboid
     ]
   end
 
+  def volume
+    @width * @height * @length
+  end
+
+  def move_to!(x, y, z)
+    @x = x
+    @y = y
+    @z = z
+  end
+
   #returns true if the two cuboids intersect each other.  False otherwise.
   def intersects?(other)
+    if self.volume > other.volume
+      smaller_cuboid = other
+      larger_cuboid = self
+    else
+      smaller_cuboid = self
+      larger_cuboid = other
+    end
+
+    smaller_cuboid.vertices.each do |point|
+      return true if larger_cuboid.point_inside?(point[:x], point[:y], point[:z])
+    end
+
+    return false
   end
 
   def point_inside?(x_other, y_other, z_other)
-    (x_other >= @x && x_other <= @x + @width || x_other <= @x && x_other >= @x + @width) &&
-    (y_other >= @y && y_other <= @y + @height || y_other <= @y && y_other >= @y + @height) &&
-    (z_other >= @z && z_other <= @z + @length || z_other <= @z && z_other >= @z + @length)
+    # detect if point is within origin and origin plus offset (also account for negative offset)
+    (x_other >= @x && x_other <= @x + @width) &&
+    (y_other >= @y && y_other <= @y + @height) &&
+    (z_other >= @z && z_other <= @z + @length)
   end
 
-  #END public methods that should be your starting point
 end
--- spec/cuboid_spec.rb
+++ spec/cuboid_spec.rb
@@ -5,16 +5,32 @@ require 'cuboid'
 #  methods as you feel need coverage
 describe Cuboid do
 
-  subject(:cuboid1) {
-    Cuboid.new({
-      x: 0,
-      y: 0,
-      z: 0,
-      width: 2,
-      height: 2,
-      length: 2,
-    })
-  }
+  subject(:cuboid1) { Cuboid.new({x: 0, y: 0, z: 0, width: 2, height: 2, length: 2}) }
+
+  subject(:cuboid2) { Cuboid.new({x: 4, y: 4, z: 0, width: 2, height: 2, length: 2}) }
+  subject(:cuboid3) { Cuboid.new({x: 1, y: 1, z: 1, width: 1, height: 1, length: 1}) }
+  subject(:cuboid4) { Cuboid.new({x: -5, y: -5, z: -5, width: 10, height: 10, length: 10}) }
+  subject(:cuboid5) { Cuboid.new({x: -5, y: -5, z: -5, width: 1, height: 1, length: 1}) }
+
+  subject(:cuboid6) { Cuboid.new({x: -10, y: -1, z: -10, width: 20, height: 2, length: 20}) }
+  subject(:cuboid7) { Cuboid.new({x: -1, y: -10, z: -1, width: 2, height: 20, length: 2}) }
+
+  describe "intersects?" do
+    it "returns true if cuboid intersects other cuboid" do
+      expect(cuboid1.intersects?(cuboid1)).to eq(true)
+      expect(cuboid1.intersects?(cuboid3)).to eq(true)
+      expect(cuboid1.intersects?(cuboid4)).to eq(true)
+      expect(cuboid4.intersects?(cuboid1)).to eq(true)
+      expect(cuboid6.intersects?(cuboid7)).to eq(true)
+      expect(cuboid7.intersects?(cuboid6)).to eq(true)
+    end
+
+    it "return false if cuboid does not intersect other cuboid" do
+      expect(cuboid1.intersects?(cuboid2)).to eq(false)
+      expect(cuboid1.intersects?(cuboid5)).to eq(false)
+      expect(cuboid2.intersects?(cuboid3)).to eq(false)
+    end
+  end
 
   describe "vertices" do
     it "returns a list of the cuboids vertices" do
@@ -37,30 +53,5 @@ describe Cuboid do
   #     # expect(subject.move_to!(1,2,3)).to be true
   #   end
   # end
-
-  describe "point_inside?" do
-    it "returns true if a point is inside the cuboid" do
-      expect(cuboid1.point_inside?(0,0,0)).to eq(true)
-      expect(cuboid1.point_inside?(0,0,2)).to eq(true)
-      expect(cuboid1.point_inside?(0,2,0)).to eq(true)
-      expect(cuboid1.point_inside?(0,2,2)).to eq(true)
-      expect(cuboid1.point_inside?(2,0,0)).to eq(true)
-      expect(cuboid1.point_inside?(2,0,2)).to eq(true)
-      expect(cuboid1.point_inside?(2,2,0)).to eq(true)
-      expect(cuboid1.point_inside?(2,2,2)).to eq(true)
-      expect(cuboid1.point_inside?(1,1,1)).to eq(true)
-    end
-
-    it "returns false if a point is outside the cuboid" do
-      expect(cuboid1.point_inside?(-1,-1,-1)).to eq(false)
-      expect(cuboid1.point_inside?(3,3,3)).to eq(false)
-      expect(cuboid1.point_inside?(0,0,5)).to eq(false)
-      expect(cuboid1.point_inside?(0,5,0)).to eq(false)
-      expect(cuboid1.point_inside?(5,0,0)).to eq(false)
-      expect(cuboid1.point_inside?(-5,0,0)).to eq(false)
-      expect(cuboid1.point_inside?(0,-5,0)).to eq(false)
-      expect(cuboid1.point_inside?(0,0,-5)).to eq(false)
-    end
-  end
 
 end
