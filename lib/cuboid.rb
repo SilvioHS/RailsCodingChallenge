86db28e73cd85a3bb2910d1562f019e26f89bfa3
Silvio Huebner-Schurch <silvio@vicinitycommerce.com>
2016-01-10 19:31:10 -0800

    more accurate intersection detection

--- lib/cuboid.rb
+++ lib/cuboid.rb
@@ -1,11 +1,13 @@
 
 class Cuboid
 
+  InvalidParamTypeError = Class.new(StandardError)
   InvalidOriginError = Class.new(StandardError)
-  InvalidDimensionsError = Class.new(StandardError)
+  InvalidDimensionError = Class.new(StandardError)
   InvalidMoveError = Class.new(StandardError)
 
 
+  # assume origin and dimensions are all integers
   def initialize(params = {})
     validate_params(params)
 
@@ -47,16 +49,16 @@ class Cuboid
 
   # returns true if the two cuboids intersect each other.  False otherwise.
   def intersects?(other)
-    if self.volume > other.volume
-      smaller_cuboid = other
-      larger_cuboid = self
-    else
-      smaller_cuboid = self
-      larger_cuboid = other
-    end
+    self_min_point = self.vertices.first
+    self_max_point = self.vertices.last
 
-    smaller_cuboid.vertices.each do |point|
-      return true if larger_cuboid.point_inside?(point[:x], point[:y], point[:z])
+    # detect if cuboid and other cuboid share a common point (integer values only)
+    (self_min_point[:x]..self_max_point[:x]).each do |x_point|
+      (self_min_point[:y]..self_max_point[:y]).each do |y_point|
+        (self_min_point[:z]..self_max_point[:z]).each do |z_point|
+          return true if other.point_inside?(x_point, y_point, z_point)
+        end
+      end
     end
 
     return false
@@ -72,10 +74,14 @@ class Cuboid
   private
 
   def validate_params(params)
+    params.each do |key, value|
+      raise InvalidParamTypeError unless value.is_a? Integer
+    end
+
     if params[:x] < 0 || params[:y] < 0 || params[:z] < 0
       raise InvalidOriginError
     elsif params[:length] <= 0 || params[:width] <= 0 || params[:height] <= 0
-      raise InvalidDimensionsError
+      raise InvalidDimensionError
     end
   end
 
--- spec/cuboid_spec.rb
+++ spec/cuboid_spec.rb
@@ -7,6 +7,28 @@ describe Cuboid do
   subject(:cuboid3) { Cuboid.new({x: 1, y: 1, z: 1, width: 1, height: 1, length: 1}) }
   subject(:cuboid4) { Cuboid.new({x: 0, y: 0, z: 0, width: 10, height: 10, length: 10}) }
   subject(:cuboid5) { Cuboid.new({x: 3, y: 3, z: 3, width: 1, height: 1, length: 1}) }
+  subject(:cuboid6) { Cuboid.new({x: 0, y: 5, z: 0, width: 20, height: 1, length: 20}) }
+  subject(:cuboid7) { Cuboid.new({x: 5, y: 0, z: 5, width: 1, height: 20, length: 1}) }
+
+  describe "initialize" do
+    it "raises error if params are not integers" do
+      expect {
+        Cuboid.new({x: 0.5, y: 0, z: 0, width: 1, height: 1, length: 1})
+      }.to raise_error(Cuboid::InvalidParamTypeError)
+    end
+
+    it "raises error if origin is negative" do
+      expect {
+        Cuboid.new({x: -1, y: 0, z: 0, width: 1, height: 1, length: 1})
+      }.to raise_error(Cuboid::InvalidOriginError)
+    end
+
+    it "raises error if dimension is less than one" do
+      expect {
+        Cuboid.new({x: 0, y: 0, z: 0, width: 0, height: -1, length: 1})
+      }.to raise_error(Cuboid::InvalidDimensionError)
+    end
+  end
 
   describe "intersects?" do
     it "returns true if cuboid intersects other cuboid" do
@@ -14,6 +36,7 @@ describe Cuboid do
       expect(cuboid1.intersects?(cuboid3)).to eq(true)
       expect(cuboid1.intersects?(cuboid4)).to eq(true)
       expect(cuboid4.intersects?(cuboid1)).to eq(true)
+      expect(cuboid6.intersects?(cuboid7)).to eq(true)
     end
 
     it "return false if cuboid does not intersect other cuboid" do
